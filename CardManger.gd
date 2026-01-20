0cc9f5c41a2f62a87d751bd3b8471f05fefd2520
wuharry <whw880218cool@gmail.com>
2026-01-20 22:56:53 +0800

    feat: add screen border and complete dragged
    class 1 from YT Barry's dev hell

--- CardManger.gd
+++ CardManger.gd
@@ -1,16 +1,29 @@
 extends Node2D
 
-
-
+const COLLISION_MASK_CARD=1
+var screen_size
 var card_being_dreeged
+
+func _ready() -> void:
+	screen_size=get_viewport_rect().size
+
+func _process(delta: float) -> void:
+	if card_being_dreeged:
+		var mouse_position =get_global_mouse_position()
+		card_being_dreeged.position=Vector2(clamp(mouse_position.x,0,screen_size.x),
+		clamp(mouse_position.y,0,screen_size.y))
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
 			print("左鍵點擊")
-			var card=raycast_check_for_card()
+			var card = raycast_check_for_card()
+			if card:
+				card_being_dreeged=card
 		else:
 			print('左鍵釋放')
+			card_being_dreeged=null
+			
 	
 #	用來檢測點擊下去的地方是否有卡片?
 func raycast_check_for_card():
@@ -27,7 +40,7 @@ func raycast_check_for_card():
 	parameters.collide_with_areas = true
 	
 	# 設定碰撞遮罩層 (只檢測第 1 層的物體)
-	parameters.collision_mask = 1
+	parameters.collision_mask = COLLISION_MASK_CARD
 	
 	# 執行點查詢,返回所有在該點的物體陣列
 	var result = space_state.intersect_point(parameters)
